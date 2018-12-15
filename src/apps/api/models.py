83b97a6abee94249be4562e49e53797e0b5ce23f
Naresh Yadav <naresh.kumar@tribe3.com>
2018-12-16 01:14:25 +0530

    common abstact models added in api module

--- src/apps/api/models.py
+++ src/apps/api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class ModelBase(models.Model):
+	"""
+	This is a abstract model class to add is_deleted, created_at and modified at fields in any model
+	"""
+	is_deleted = models.BooleanField(default=False)
+	created_at = models.DateTimeField(auto_now_add=True)
+	modified_at = models.DateTimeField(auto_now=True)
+
+	class Meta:
+		abstract = True
+
+	def delete(self, *args, **kwargs):
+		""" Soft delete """
+		self.is_deleted = True
+		self.save()
+
+class AddressDetails(models.Model):
+	"""
+	Model to store - Address Details
+	"""
+	address = models.TextField()
+	landmark = models.CharField(max_length=255, null=True, blank=True)
+	city = models.CharField(max_length=255, null=True, blank=True)
+	pincode = models.CharField(max_length=6, null=True, blank=True)
+	state = models.CharField(max_length=255, null=True, blank=True) # for this values can be binded as well
