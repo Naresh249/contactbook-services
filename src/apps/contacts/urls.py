53e37964e562731f7ae56ef98758b8b6092f2587
Naresh Yadav <naresh.kumar@tribe3.com>
2018-12-16 16:55:59 +0530

    routes added for API

--- src/apps/contacts/urls.py
+++ src/apps/contacts/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^contact-manager/$', views.ContactDetails.as_view(), name='contact-manager'),
+	url(r'^contact-manager/$', views.ContactDetailsManager.as_view(), name='contact-manager'),
 	# url(r'^search/$', views.SearchContact.as_view(), name='search_contact'),
 ]
